8b4650c2c7c65882e2c6c9714682c6f3509d9524
dominicriordan <d.riordan@sensysgatso.com>
2023-04-14 16:58:01 +0100

    fix

--- monitor.go
+++ monitor.go
@@ -67,8 +67,10 @@ loop:
 				break loop
 			}
 
-			if m.c.config.requests != 0 && stats.totalRequests >= 10 && stats.totalRequests%(m.c.config.requests/10) == 0 {
-				fmt.Printf("Completed %d requests\n", stats.totalRequests)
+			if m.c.config.requests != 0 && stats.totalRequests >= 10 {
+				if stats.totalRequests%(m.c.config.requests/10) == 0 {
+					fmt.Printf("Completed %d requests\n", stats.totalRequests)
+				}
 			} else if m.c.config.requests == 0 && stats.totalRequests >= 10 && stats.totalRequests%(1000) == 0 {
 				fmt.Printf("Completed %d requests\n", stats.totalRequests)
 			}
